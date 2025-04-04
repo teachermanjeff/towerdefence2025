302d22594d4c518e39af1a0990cb5ed516a3b08e
Josh <1301545@sd61learn.ca>
2025-04-04 09:09:37 -0700

    Resolving

--- Scripts/test.gd
+++
@@ -1,9 +0,0 @@
-extends CharacterBody2D
-
-# Export Vars
-@export var path : PathFollow2D
-@export var speed : float 
-
-# Movement
-func _process(delta: float) -> void:
-	path.progress = path.progress + speed
