0b76897243b81c95b73721d8b2d31a1369176b09
Josh <1301545@sd61learn.ca>
2025-04-07 12:49:33 -0700

    Working Placement Mechanic With Limited Logic

--- Scripts/ui.gd
+++ Scripts/ui.gd
@@ -1,9 +1,31 @@
 extends Control
 
-signal is_placable(placable, not_placable)
+# Export Vars
+@export var placable_area : Area2D
 
+# Onready Vars
+@onready var test_tower : PackedScene = preload("res://Scenes/test_tower.tscn")
+
+# Vars
+var is_placable = false
+var is_picked_up = false
+
+func _process(delta: float) -> void:
+	# Get global mouse position
+	var mouse_pos = get_global_mouse_position()
+
+	# If the cursor is within a placable area and the player has a tower selected place said tower at cursor position
+	if is_placable == true and Input.is_action_just_pressed("Left Click") and is_picked_up == true:
+		var test_tower_temp = test_tower.instantiate()
+		test_tower_temp.global_position = mouse_pos
+		add_child(test_tower_temp)
+
+# Set is_placable true on mouse entering placable area 
 func _on_area_2d_mouse_entered() -> void:
-	emit_signal("placable")
+	is_placable = true
+	print(is_placable)
 
+# Set is_placable false on mouse entering placable area
 func _on_area_2d_mouse_exited() -> void:
-	emit_signal("not_placable")
+	is_placable = false
+	print(is_placable)
